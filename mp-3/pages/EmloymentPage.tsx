7dc734082d4ebb87d616870f953e81aa8705bb88
ayagoobi12 <ayagoobi@bu.edu>
2025-07-21 22:15:43 -0400

    Fixed capitalization error for deployment

--- mp-3/pages/EmloymentPage.tsx
+++ mp-3/pages/EmloymentPage.tsx
@@ -2,7 +2,7 @@ import Header from "../components/Header";
 import Nav from "../components/Nav";
 import Footer from "../components/Footer";
 import Main from "../components/Main";
-import "./employment.css";
+import "./Employment.css";
 
 export default function EmpPage() {
   return (
